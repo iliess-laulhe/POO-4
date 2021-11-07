65628d4a90d2c42d3ada86abbaf211999544e640
iliess laulhe <iliesslaulhe@icloud.com>
2021-11-07 11:37:26 +0100

    quete finish

--- Bicycle.php
+++ Bicycle.php
@@ -1,7 +1,22 @@
 <?php
 
 require_once 'Vehicule.php';
+require 'LightableInterface.php';
 
-class Bicycle extends Vehicule
+class Bicycle extends Vehicule implements LightableInterface
 {
+
+
+    public function switchOff(): bool
+    {
+        return false;
+    }
+
+    public function switchOn(): bool
+    {
+        if ($this->getCurrentSpeed() > 10) {
+            return true;
+        }
+        return false;
+    }
 }
--- Car.php
+++ Car.php
@@ -1,6 +1,14 @@
 <?php
-class Car
+require_once 'Vehicule.php';
+require_once 'LightableInterface.php';
+
+ class Car extends Vehicule implements LightableInterface
+
 {
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
     private $color;
     private   $nbWheels;
     private   $nbSeats ;
@@ -66,5 +74,15 @@ class Car
     {
         return $this->hasParkBrake;
     }
+     public function switchOn(): bool
+     {
+
+         return true;
+     }
+     public function switchOff() : bool
+     {
+
+         return false;
+     }
 
 }
